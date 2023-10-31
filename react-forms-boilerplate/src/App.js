1e89793fc41eb24a8ec44442d4363ece656a7b11
Preeti Aggarwal <149458796+PreetiAggarwal060787@users.noreply.github.com>
2023-10-31 15:39:51 +0530

    Add files via upload

---
+++ react-forms-boilerplate/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Calculator from './calculator/calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>learn react</h1>
+    <Calculator></Calculator>
+    </div>
+  );
+}
+
+export default App;
